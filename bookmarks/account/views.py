b3ef8c9ad28c441d07ce6ee1c94413c85fda50f1
Denis Malorodov <denis.malod@mail.ru>
2025-09-23 15:27:41 +1000

    Редактирование профиля пользователя

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,6 +3,51 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm
+from .models import Profile
+from .forms import LoginForm, UserRegistrationForm, \
+                   UserEditForm, ProfileEditForm
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile)
+    return render(request,
+                'account/edit.html',
+                {'user_form': user_form,
+                'profile_form': profile_form})
+
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создать новый объект пользователя,
+            # но пока не сохранять его
+            new_user = user_form.save(commit=False)
+            # Установить выбранный пароль
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Сохранить объект User
+            new_user.save()
+            # Создать профиль пользователя
+            Profile.objects.create(user=new_user)
+
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
 
 
 @login_required
@@ -29,25 +74,3 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, "account/login.html", {"form": form})
-
-
-def register(request):
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            # Создать новый объект пользователя,
-            # но пока не сохранять его
-            new_user = user_form.save(commit=False)
-            # Установить выбранный пароль
-            new_user.set_password(
-                user_form.cleaned_data['password'])
-            # Сохранить объект User
-            new_user.save()
-            return render(request,
-                          'account/register_done.html',
-                          {'new_user': new_user})
-    else:
-        user_form = UserRegistrationForm()
-    return render(request,
-                  'account/register.html',
-                  {'user_form': user_form})
