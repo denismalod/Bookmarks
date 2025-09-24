ae4b622f041588d84a8ca91c99430650ba482c52
Denis Malorodov <denis.malod@mail.ru>
2025-09-24 16:42:47 +1000

    Создание профиля пользователя социальной аутентификации

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -1,4 +1,12 @@
 from django.contrib.auth.models import User
+from account.models import Profile
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Создать профиль пользователя для социальной аутентификации
+    """
+    Profile.objects.get_or_create(user=user)
+
 
 
 class EmailAuthBackend:
