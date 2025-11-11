9ad9a246b31a8fee012bdd4cf56e939e45ad6459
Denis Malorodov <denis.malod@mail.ru>
2025-11-12 00:24:32 +1000

    feat(account): Implement AJAX user follow/unfollow system

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,39 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm
-from .models import Profile
+from .models import Profile, Contact
 from .forms import UserEditForm, ProfileEditForm
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
+
 
 @login_required
 def edit(request):
-    if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(
-                                    instance=request.user.profile,
-                                    data=request.POST,
-                                    files=request.FILES)
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Profile updated '\
-                                      'successfully')
+            messages.success(request, "Profile updated " "successfully")
         else:
-            messages.error(request, 'Error updating your profile')
-
+            messages.error(request, "Error updating your profile")
 
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(
-                                    instance=request.user.profile)
-    return render(request,
-                'account/edit.html',
-                {'user_form': user_form,
-                'profile_form': profile_form})
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        "account/edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
 
 
 def register(request):
@@ -96,3 +94,21 @@ def user_detail(request, username):
     return render(
         request, "account/user/detail.html", {"section": "people", "user": user}
     )
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
+        except User.DoesNotExist:
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
